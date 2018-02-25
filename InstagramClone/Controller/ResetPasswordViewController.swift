696c1aa7aa70228c6f972c35983fa51945aef100
Mihai Ruber <mruber@ucsd.edu>
2018-02-24 19:25:15 -0800

    resetpass

--- InstagramClone/Controller/ResetPasswordViewController.swift
+++ InstagramClone/Controller/ResetPasswordViewController.swift
@@ -7,20 +7,52 @@
 //
 
 import UIKit
+import Parse
+import IQKeyboardManagerSwift
 
 class ResetPasswordViewController: UIViewController {
-
+    
+    // Variables
+    
+    // Outlets
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var resetBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
+    // Actions
 
+    @IBAction func resetBtnClicked(_ sender: Any) {
+        if (emailTxt.text?.isEmpty == true) {
+            let alert = UIAlertController(title: "Error", message: "Fill in all the fields", preferredStyle: UIAlertControllerStyle.alert)
+            let ok = UIAlertAction(title: "Ok", style: UIAlertActionStyle.cancel, handler: nil)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+        }
+        self.view.endEditing(true)
+        
+        PFUser.requestPasswordResetForEmail(inBackground: emailTxt.text!) { (success: Bool, error: Error?) in
+            if success {
+                let alert = UIAlertController(title: "Email for reseting password", message: "has been sent to texted email", preferredStyle: UIAlertControllerStyle.alert)
+                let ok = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { (UIAlertAction) in
+                    self.dismiss(animated: true, completion: nil)
+                })
+                alert.addAction(ok)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                print(error?.localizedDescription)
+            }
+        }
+    }
+    
+    @IBAction func cancelBtnClicked(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 
 }
